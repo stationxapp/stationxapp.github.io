61de4d6e194c0304e6ac4e0adf65d181cf101e0e
StationX Developer <stationx.app@gmail.com>
2021-06-22 11:39:35 +0800

    commit

--- git_add.sh
+++ git_add.sh
@@ -1,2 +1,6 @@
-git add dashboard/index.html
 git add git_add.sh
+git add jq_securities/assets/js/app.min.js
+git add jq_securities/handlebars/footer_en.handlebars
+git add jq_securities/handlebars/footer_hk.handlebars
+git add jq_securities/templates/footer_en.precompiled.js
+git add jq_securities/templates/footer_hk.precompiled.js
